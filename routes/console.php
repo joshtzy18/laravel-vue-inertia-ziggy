0e7703cefdcc3214212723228fed17cb367c1b08
joshtzy18 <joshtzybugs@gmail.com>
2026-01-11 09:24:22 +0800

    my starter fresh

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,34 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+// use Illuminate\Foundation\Inspiring;
+// use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+// Artisan::command('inspire', function () {
+//     $this->comment(Inspiring::quote());
+// })->purpose('Display an inspiring quote')->hourly();
+
+
+// mailtrap
+# app/routes/console.php
+# php artisan send-mail
+
+use Illuminate\Support\Facades\Artisan;
+use Mailtrap\Helper\ResponseHelper;
+use Mailtrap\MailtrapClient;
+use Mailtrap\Mime\MailtrapEmail;
+use Symfony\Component\Mime\Address;
+
+Artisan::command('send-mail', function () {
+    $email = (new MailtrapEmail())
+        ->from(new Address('hello@demomailtrap.co', 'Mailtrap Test'))
+        ->to(new Address('joshtzybugs@gmail.com'))
+        ->subject('You are awesome!')
+        ->category('Integration Test')
+        ->text('Congrats for sending test email with Mailtrap!');
+
+    $response = MailtrapClient::initSendingEmails(
+        apiKey: '<YOUR_API_TOKEN>'
+    )->send($email);
+
+    var_dump(ResponseHelper::toArray($response));
+})->purpose('Send Mail');
